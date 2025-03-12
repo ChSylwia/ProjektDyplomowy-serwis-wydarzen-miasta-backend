623d8435a29c413d38908ee14942779f9b420c8d
Sylwia <chedkowska0@gmail.com>
2025-03-12 18:47:49 +0100

    debug

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
+file_put_contents('/tmp/debug.log', $_SERVER['REQUEST_URI'] . PHP_EOL, FILE_APPEND);
+
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
